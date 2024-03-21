6637af2c976e0c889dcf897bf26df57977c3b470
RAHUL KUMAR <77164649+RahulKumar-tech@users.noreply.github.com>
2024-03-22 00:36:55 +0530

    Add files via upload

---
+++ ifElse_OddEven.cpp
@@ -0,0 +1,17 @@
+// Q) Take positive integers input and tell if it is even or odd.
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Enter a number : ";
+    int n;
+    cin>>n;
+    if(n%2==0){
+        cout<<"even"<<endl;
+    }
+    else{
+        cout<<"odd";  
+    }
+    //    if(n%2!=0){
+    //     cout<<"odd";   //Enter a number : 55 odd
+    // } 
+ }
